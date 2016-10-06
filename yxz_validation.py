718bfe71cd02046987851bafcc814caf112dd2f7
Justin Bloomfield <jbloomfield@live.com>
2016-10-06 01:35:34 +0000

    Justin: corrected regex expression

--- yxz_validation.py
+++ yxz_validation.py
@@ -1,7 +1,10 @@
-import re
+import re #for regex checking 
 
 def yxz_test(path):
-    valid_chars = re.compile('[0-9-]')
+    """
+    Takes the path to the file, and checks that it contains the correct number of entries per line and only valid characters. Quits when file is found to be invalid, stating the error.
+    """
+    valid_chars = re.compile('[0-9\.\-]') #characters 0 through 9 
     with open(path, "r") as test:
         for line in test:
             blocks = line.split()
@@ -18,4 +21,3 @@ def yxz_test(path):
     print("Valid file")
     return True
 
-yxz_test("data_files/au1k.txt")
