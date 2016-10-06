c7bf9faa73dc11aa7091711454d3ec28758bf139
Justin Bloomfield <jbloomfield@live.com>
2016-10-06 01:57:33 +0000

    Justin: comments/docstring added to validation

--- yxz_validation.py
+++ yxz_validation.py
@@ -1,16 +1,16 @@
-import re #for regex checking 
+import re # for regex checking 
 
 def yxz_test(path):
     """
     Takes the path to the file, and checks that it contains the correct number of entries per line and only valid characters. Quits when file is found to be invalid, stating the error.
     """
-    valid_chars = re.compile('[0-9\.\-]') #characters 0 through 9 
-    with open(path, "r") as test:
-        for line in test:
+    valid_chars = re.compile('[0-9\.\-]') # characters 0 through 9 
+    with open(path, "r") as test: 
+        for line in test: # test each line of file
             blocks = line.split()
-            if len(blocks) == 3:
+            if len(blocks) == 3: # tests for correct number of entries in file
                 for item in blocks:
-                    if valid_chars.match(item):
+                    if valid_chars.match(item): # regex matching
                         pass
                     else:
                         print("Invalid characters")
