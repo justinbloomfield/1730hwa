c2d33eef18e7c48fbfe9f823a2a9f89ef283862d
Justin Bloomfield <jbloomfield@live.com>
2016-10-10 20:12:31 +0000

    Justin: it lives! now draws a (correct) graph

--- funct_2.py
+++ funct_2.py
@@ -1,4 +1,3 @@
-####### ------ REMEMBER TO CHECK IF WE SHOULD INCLUDE THE 0 SEA LEVEL STUFF IN THE CALCULATION OF CURRENT????????
 ####### ------ DO WE NEED TO ACCOUNT FOR NEGATIVE SEA ????
 
 #┌──────────────┐
@@ -7,25 +6,20 @@
 
 import re # for regex checking 
 import numpy as np # linspace, as range() can't use floats
-#import matplotlib.pyplot as plt # for graphing
+import matplotlib.pyplot as plt # for graphing
 
 # user inputs datafile, L, mean_vert_disp, and mean_horiz_disp
 #assignment of user inputs
-#path = input("Please enter the path to the desired data file for analysis: ")
-#mean_vert_dist = float(input("Please enter the mean vertical spacing for the provided data file: "))
-#mean_horiz_dist = float(input("Please enter the mean horizontal spacing for the provided data file: "))
-#sea_rise = float(input("Please enter a sea level height for remaining land area analysis: ")) 
+path = input("Please enter the path to the desired data file for analysis: ")
+mean_vert_dist = float(input("Please enter the mean vertical spacing for the provided data file: "))
+mean_horiz_dist = float(input("Please enter the mean horizontal spacing for the provided data file: "))
+sea_rise = float(input("Please enter a sea level height for remaining land area analysis: ")) #maybe an exception catcher here if we decide we want to account for user to not enter anything
 
 
 #assignment of file object to user-provided file
-path = "data_files/sydney250m.txt"
 datafile = open(path, 'r')
 data_array = []
 
-
-mean_vert_dist = 0.278
-mean_horiz_dist = 0.231
-sea_rise = 2
 #┌───────────────────┐
 #│ --- Functions --- │
 #└───────────────────┘
@@ -59,80 +53,71 @@ def validate(path): # maybs change this to read from data_array so the file does
 # validate file
 validate(path)
 
-def main(L, mean_vert, mean_horiz):
+    
+def calc_area(L, mean_vert, mean_horiz, array):
     """
     calculates the area above sea level L, with mean vertical and horizontal spacing given
     """
+    area = 0
+    count = 0
+    for item in array:
+        if float(item[2]) > L:
+            count += 1
+
+    area = count * mean_vert * mean_horiz
+    return area
+    
+def zero_rise(L, mean_vert, mean_horiz, array): #sea rise, array of data, height list and area list
+    '''
+    performs function level 2 operations (i.e. when no sea rise is given)
+    '''
     height_list = []
     area_list = []
-    empty_L = False
     max_alt = 0
-    count = 0
 
-    #for line in datafile:
-    #    data_array.append(line.split())
+    for item in array: # find highest altitude in data
+        if float(item[2]) > max_alt:
+            max_alt = float(item[2])
+
+    step_num = np.linspace(0, max_alt, 100)
+
+    for step in step_num:
+        height_list.append(step)
+
+    for alt in height_list:
+        area = calc_area(alt, mean_vert, mean_horiz, array)
+        area_list.append(area)
+
+    return height_list, area_list
+
+def tier1_disp_result(L, mean_vert, mean_horiz): # shows function level 1  data.
+    current = calc_area(0, mean_vert, mean_horiz, data_array)
+    absolute = calc_area(L, mean_vert, mean_horiz, data_array)
+    percentage = (absolute/current) * 100
+    
+    print("At %0.0f metre(s) above sea level, there will be %0.3f square kilometres of land, which is %0.3f percent of the current value" % (L, absolute, percentage))
+    return True
+
+def tier2_disp_result():
+
+    height_list, area_list = zero_rise(L, mean_vert, mean_horiz, array)
+    graph_plot(height_list, area_list)
+
+    return True
+
+def main(L, mean_vert, mean_horiz, array): #put everything together!
+    empty_L = False
 
     if L == 0:
         empty_L = True
 
-
-    for item in data_array:
-        if float(item[2]) > L:
-            count += 1
-        if empty_L == True:
-            if float(item[2]) > max_alt:
-                max_alt = float(item[2])
-            else:
-                pass
-    area = count * mean_vert * mean_horiz 
-
     if empty_L == True:
-        #step_size = max_alt / 100
-        step_num = np.linspace(0, max_alt, 100)
-        #print(step_size)
-    #    #val = 0
-    #    #while val < max_alt:
-    #        ### THIS ARE WRONG BURJ KHALIFA       
-    #    #val = max_alt
-    #     
-        for step in step_num: 
-            for item in data_array:
-                if float(item[2]) > L:
-                    area_two = count * mean_vert * mean_horiz
-            area_list.append(area_two)
-            count += 1
+        height_list, area_list = zero_rise(L, mean_vert, mean_horiz, array)
+        graph_plot(height_list, area_list)
+    else:
+        tier1_disp_result(L, mean_vert, mean_horiz)
 
-        for step in step_num:
-            height = step
-            height_list.append(step)
-            #calc area for these, then display (maybe a separate function? this one is getting pretty big...)
-    #print(area)
-    (area_list)
-    (height_list)
-    return area
-    
-#def calc_area(L, mean_vert, mean_horiz, data):
-#    for item in data:
-#        if float(item[2]) > L:
-#            count += 1
-
-#    area = count * mean_vert * mean horiz
-#    return area
-    
-
-def disp_result(L, mean_vert, mean_horiz):
-    current = main(0, mean_vert, mean_horiz)
-    absolute = main(L, mean_vert, mean_horiz)
-    percentage = (absolute/current) * 100
-    
-    print("At %0.0f metre(s) above sea level, there will be %0.3f square kilometres of land, which is %0.3f percent of the current value" % (L, absolute, percentage))
-
-    #percent_list = []
-    #for item in area_list:
-    #    percentage = (item/area_list[0]) * 100
-    #    percent_list.append(percentage)
-
-def graph_plot(hl, pl): 
+def graph_plot(al, pl): 
     '''
     Plots data for function level 2 (this is a shit description and
     should be fixed
@@ -140,21 +125,14 @@ def graph_plot(hl, pl):
     plt.title("Sea level rise vs remaining land area")
     plt.xlabel("Sea level rise (m)")
     plt.ylabel("Remaining land area (km^2)")
-    plt.plot(height_list, percent_list, "b")
+    plt.plot(al, pl, "b")
     plt.show()
 
-#def tier2_disp_result():
-#    for index in range(len(height_list)):
-#        printheight = height_list(index)
-#        printarea = area_list(index)
-#        printpercent = percent_list(index)
-#        print("At %0.0f metres above sea level, there will be %0.3f square kilometres of land, which is %0.3f percent of the current value" % (printheight, printarea, printpercent))
-#        
-#tier2_disp_result()
         
-#calc_area(mean_vert_dist, mean_horiz_dist)
-disp_result(sea_rise, mean_vert_dist, mean_horiz_dist)
-
+#┌──────────────┐
+#│ --- RUN! --- │
+#└──────────────┘
+main(sea_rise, mean_vert_dist, mean_horiz_dist, data_array)
 
 
 
