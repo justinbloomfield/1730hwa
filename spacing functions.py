163efd00593d95bb97867415379f3f3b13968125
supermathu <u6018764@anu.edu.au>
2016-10-11 22:54:34 +1100

    Finished draft version of function

--- spacing functions.py
+++ spacing functions.py
@@ -1,1 +1,21 @@
-def 
+mean_horiz_dist = spacing(1)
+mean_vert_dist = spacing(2)
+
+def spacing(i):
+  diff_list = []
+  col_entries = []
+  for line in datafile:
+    col_entries.append(line[i])
+  for num in range(len(col_entries)-1):
+    difference = col_entries[num] - col_entries[num+1]
+    if difference <= 0:
+      difference *= -1
+    diff_list.append(difference)
+  total = 0
+  for val in diff_list:
+    total += x
+    return total
+  elem_count = len(diff_list)
+  mean_spacing = total/elem_count
+  return mean_spacing
+
