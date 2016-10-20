e7ed2c8c97df2839bec6dfd594e82abdcc128c6a
supermathu <u6018764@anu.edu.au>
2016-10-20 12:25:06 +1100

    Added docstrings

--- funct_1.py
+++ funct_1.py
@@ -24,8 +24,15 @@ data_list = []
 
 
 def validate(path): # maybs change this to read from data_list so the file doesn't have to be scanned twice, for speed. Not imperative.
-    """
-    Takes the path to the file, and checks that it contains the correct number of entries per line and only valid characters. Quits when file is found to be invalid, stating the error.
+    """Checks whether the filepath refers to a valid YXZ format file. 
+    
+    Retrieves the file located at the given filepath. Opens that file, and checks that
+    the file contains the correct number of entries per line, and only valid characters
+    in each entry. If the file is found to be invalid, the program quits and states the
+    relevant error.
+    
+    Args:
+        testing_file: The path to the file for validation.
     """
     valid_chars = re.compile('[0-9\.\-]') # characters 0 through 9 
     with open(path, "r") as test: 
@@ -49,8 +56,21 @@ def validate(path): # maybs change this to read from data_list so the file doesn
 validate(path)
 
 def calc_area(L, mean_vert, mean_horiz):
-    """
-    calculates the area above sea level L, with mean vertical and horizontal spacing given
+    """Finds land area, as per first approximation.
+    
+    Args:
+        L: A numeric value representing sea level rise. 
+        mean_vert: The numeric value for mean vertical spacing of
+            data values in datafile.
+        mean_horiz: The numeric value for mean horizontal spacing 
+            of data values in datafile.
+        array: The array containing line-split data values
+            from datafile.
+            
+    Returns:
+        A float value representing the absolute area of land above sea 
+        level (in square kilometres) for a given value of L, as per the
+        first approximation. 
     """
     count = 0
     for line in datafile:
@@ -62,6 +82,11 @@ def calc_area(L, mean_vert, mean_horiz):
     return area
 
 def disp_result():
+    """Displays required output for first approximation.
+    
+    Prints absolute and relative land area remaining for a 
+    given sea level rise.
+    """
     current = calc_area(0, mean_vert_dist, mean_horiz_dist)
     absolute = calc_area(sea_rise, mean_vert_dist, mean_horiz_dist)
     percentage = (absolute/current) * 100
@@ -70,12 +95,3 @@ def disp_result():
     print("At %0.0f metres above current sea level, there will be %0.3f square kilometres of land, which is %0.3f percent of the current value" % (sea_rise, absolute, percentage))
 
 disp_result()
-
-#┌────────────────┐
-#│ --- HCD4T ---  │
-#└────────────────┘
-
-#datafile = open("data_files/sydney250m.txt","r")
-#mean_vert_dist = 0.278
-#mean_horiz_dist = 0.231
-#sea_rise = 13.000
